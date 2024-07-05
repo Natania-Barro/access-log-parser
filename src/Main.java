a1af0e55fd99378f4b85502d92c4e34b118754bb
tatiana <t-belyakova@yandex.ru>
2024-07-05 12:53:55 +0300

    Number-experiments

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("“Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
-    }
+        System.out.println("“Введите число:");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("“Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        int sum = number+number2;
+        System.out.println("Сумма введенных чисел = "+sum+";");
+        int difference = number-number2;
+        System.out.println("Разность введенных чисел = "+difference+";");
+        int composition = number*number2;
+        System.out.println("Преремножение введенных чисел = "+composition+";");
+        double division = (double) number/number2;
+        System.out.println("Результат деление введенных чисел = "+division+".");
+        }
 }
