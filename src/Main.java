9e3b25fb71ea47e0fa8776e20c9b62b72da6371f
tatiana <t-belyakova@yandex.ru>
2024-07-19 11:38:40 +0300

    Adding an infinite loop to open a file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,41 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("“Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        System.out.println("Длина текста: " + text.length());
+
+
+        Scanner scanner = new Scanner(System.in);
+        int validFileCount = 0;
+
+        while (true) {
+            System.out.println("Укажи путь к файлу:");
+            String path = scanner.nextLine();  // Используем один экземпляр Scanner
+
+            if (path.equalsIgnoreCase("exit")) {
+                System.out.println("Выход");
+                break;
+            }
+
+            File file = new File(path);
+            boolean fileExists = file.exists();  // Проверяем, существует ли файл
+            boolean isDirectory = file.isDirectory();  // Проверяем, является ли путь директорией
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Это путь к папке или к несуществующему пути.");
+                System.out.println("Ты ввел херню, напиши точный путь к файлу!");
+                System.out.println("__________________________________________");
+                continue;  // Продолжаем цикл
+            } else {
+                validFileCount++;
+                System.out.println("Путь указан верно!");
+                System.out.println("Этот файл № " + validFileCount);
+            }
+        }
     }
 }
+
